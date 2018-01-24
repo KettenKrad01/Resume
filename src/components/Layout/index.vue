053f9b67a6fdebf11b78c152c9ddc2f86f4a74d8
YanChaoying <371777394@qq.com>
2018-01-24 09:01:52 +0800

    start

---
+++ src/components/Layout/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="rm-layout">
+        <el-card>
+            <slot>content</slot>
+        </el-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {};
+        }
+    };
+</script>
+
+<style lang="less" scoped>
+    .rm-layout{
+        width: 1124px;
+        margin: 50px auto;
+        background: #fff;
+    }
+</style>
---
+++ src/components/Main/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="rm-main">
+        <div class="rm-main-left">
+            <main-left></main-left>
+        </div>
+        <div class="rm-main-right">个人</div>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import Left from './Left';
+
+    Vue.component('main-left', Left);
+    export default {
+        data() {
+            return {};
+        }
+    };
+</script>
+
+<style lang="less" scoped>
+    .rm-main {
+        display: flex;
+        .rm-main-left {
+            flex: 0.65;
+            padding: 10px 15px;
+            overflow: hidden;
+        }
+        .rm-main-right {
+            flex: 0.35;
+            padding: 10px 15px;
+            overflow: hidden;
+        }
+    }
+</style>
